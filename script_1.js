bad873ad0d6afe866145ccf930f8d680a6e00bac
Raphael Silva Ramalho <70926962+Ieafyy@users.noreply.github.com>
2023-06-11 18:55:54 -0300

    Add files via upload

---
+++ script_1.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+params = new URLSearchParams(window.location.search)
+id = params.get('id')
+console.log(id)
+
+$.ajax({
+    url: 'https://diwserver.vps.webdock.cloud/products/' + id,
+    method: 'GET',
+    success: (e) => {
+        console.log(e)
+        $('#foto').attr('src', e['image'])
+        $('#titulo').text(e['title'])
+        $('#preco').text("R$"+((e['price']).toFixed(2)).toString().replace('.',','))
+        $('#desc').html(e['description'])
+        document.title = e['title']
+    },
+    error: (e) => {
+        console.log(e)
+    }
+})
+
+$('#seta').click(() => {
+    window.location.href = 'index.html'
+})
+
+
+
+})
